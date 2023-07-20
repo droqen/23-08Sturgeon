e95d29087ad85a10d0e4be8274c7ef9d94d6fbe4
Alexander Martin <hi@droqen.com>
2023-07-20 00:35:59 -0400

    w10: fixed: findmycam returns cam (instead of player)

--- w10_skatemouse/src/c_findmycam.rs
+++ w10_skatemouse/src/c_findmycam.rs
@@ -3,14 +3,14 @@ use ambient_api::prelude::*;
 use ambient_api::components::core::player::{user_id, local_user_id};
 use crate::components::plr_glidercam;
 
-pub fn build_query() -> GeneralQuery<Component<String>> {
-    query(user_id()).requires(plr_glidercam()).build()
+pub fn build_query() -> GeneralQuery<(Component<EntityId>, Component<String>)> {
+    query((plr_glidercam(), user_id())).build()
 }
 
-pub fn try_find_my_cam(query : GeneralQuery<Component<String>>) -> Option<EntityId> {
+pub fn try_find_my_cam(query : GeneralQuery<(Component<EntityId>, Component<String>)>) -> Option<EntityId> {
     let local_uid =
         entity::get_component(entity::resources(), local_user_id()).unwrap();
-    for (cam, uid) in query.evaluate() {
+    for (plr, (cam,uid)) in query.evaluate() {
         if uid == local_uid { return Some(cam); }
     }
     return None; // no camera found
