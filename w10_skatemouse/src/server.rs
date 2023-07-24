647c38db07621424a5b3a674be7eba34135af0dc
Alexander Martin <hi@droqen.com>
2023-07-24 16:38:27 -0400

    server uses alternate player controller - v2

--- w10_skatemouse/src/server.rs
+++ w10_skatemouse/src/server.rs
@@ -1,7 +1,7 @@
 use ambient_api::prelude::main;
 
 mod s_selfie;
-mod s_playergliderphysics;
+mod s_playergliderphysics_v2;
 mod s_steering_and_camera;
 mod s_mkworld;
 
@@ -9,7 +9,7 @@ mod s_mkworld;
 pub fn main() {
 
     crate::s_selfie::setup();
-    crate::s_playergliderphysics::setup();
+    crate::s_playergliderphysics_v2::setup();
     crate::s_steering_and_camera::setup_mouse_control();
     crate::s_mkworld::setup_world_entities();
 
