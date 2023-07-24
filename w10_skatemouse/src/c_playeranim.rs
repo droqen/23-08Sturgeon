2ebea4a15b187fa7e62d1e41a44bb5aa70151e19
Alexander Martin <hi@droqen.com>
2023-07-24 16:39:07 -0400

    animate the player differently - model is not a child

--- w10_skatemouse/src/c_playeranim.rs
+++ w10_skatemouse/src/c_playeranim.rs
@@ -5,6 +5,7 @@ use ambient_api::{
         app::name,
         transform::{local_to_parent, local_to_world, translation, rotation, scale},
         primitives::cube,
+        rendering::color,
     },
     prelude::*,
 };
@@ -21,21 +22,23 @@ pub fn setup() {
                 .with(cube(), ())
                 .with(translation(), Vec3::ZERO)
                 .with(rotation(), Quat::IDENTITY)
-                .with(scale(), vec3(0.5, 0.5, 1.0))
+                .with(scale(), vec3(0.60, 0.60, 1.0))
+                .with(color(), random::<Vec3>().extend(1.0))
                 .with(animodel_lookdir(), vec3(1., 0., 0.))
                 .with(animodel_yaw(), 0.)
                 .with(animodel_yawvel(), 10.) // spawn spinning
-                .with(local_to_parent(), Mat4::IDENTITY)
+                // .with(local_to_parent(), Mat4::IDENTITY)
                 .spawn();
 
-            entity::add_child(glider, animodel);
-            entity::add_component(glider, local_to_world(), Mat4::IDENTITY);
+            // entity::add_child(glider, animodel);
+            // entity::add_component(glider, local_to_world(), Mat4::IDENTITY);
             entity::add_component(glider, glider_animodel(), animodel);
         }
     });
 
     query((glider_animodel(), translation(), glider_landvel(), glider_hook_pos())).each_frame(|gliders|{
         for (glider, (animodel, pos, landvel, hookpos)) in gliders {
+            entity::set_component(animodel, translation(), pos); // animodel position = my position
             // let model_yaw = entity::get_component(animodel, animodel_yaw()).expect("Animodel has no 'yaw'");
             let a : Vec3 = landvel.extend(0.0);
             let b : Vec3 = hookpos - pos;
@@ -45,7 +48,7 @@ pub fn setup() {
             if desired_lookdir.length_squared() > 0.2 {
                 desired_lookdir = desired_lookdir.normalize();
                 entity::set_component(animodel, animodel_lookdir(), desired_lookdir);
-                entity::set_component(animodel, rotation(), Quat::from_rotation_arc(vec3(0., -1., 0.), desired_lookdir));
+                entity::set_component(animodel, rotation(), Quat::from_rotation_arc_2d(vec2(0., -1.), desired_lookdir.truncate()));
                 // let desired_yawvel = shortest_yaw_delta(model_yaw, dir2yaw(desired_lookdir));
                 // println!("Yaw vel from yaw{:?} to yaw{:?} is vel{:?}", model_yaw, dir2yaw(desired_lookdir), desired_yawvel);
                 // entity::mutate_component(animodel, animodel_yawvel(), move |yawvel|{
