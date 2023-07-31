9e851b85f932eba8cd9ba9f9865ea65a70e0df58
Alexander Martin <hi@droqen.com>
2023-07-31 09:27:25 -0400

    generate buildings, slight reorg of mkworld. unfinished

--- w10_skatemouse/src/s_mkworld.rs
+++ w10_skatemouse/src/s_mkworld.rs
@@ -1,106 +1,115 @@
 
 fn lerp(a:f32,b:f32,t:f32) -> f32 { a*(1.-t)+b*t }
 
-pub fn setup_world_entities() {
-    // for i in 0..10+1 {
-    //     let depth = i as f32 * 0.1;
-    //     Entity::new()
-    //         .with(name(), "Water layer".to_string())
-    //         .with_merge(make_transformable())
-    //         .with(quad(), ())
-    //         .with(scale(), vec3(999., 999., 1.0))
-    //         .with(transparency_group(), 1)
-    //         .with(color(), vec4(
-    //             lerp(0.1, 0.0, depth), 
-    //             lerp(0.2, 0.0, depth), 
-    //             lerp(0.7, 0.0, depth), 
-    //             lerp(0.1, 1.0, depth),
-    //         ))
-    //         .with(translation(), vec3(0., 0., lerp(0.5, -0.5, depth)))
-    //         .spawn();
-    // }
-    Entity::new()
-        .with(name(), "Water (1)".to_string())
-        .with_merge(make_transformable())
-        .with(scale(), vec3(999., 999., 1.0))
-        .with(transparency_group(), 1)
-        .with(color(), vec4(0.4, 0.6, 0.9, 0.8))
-        .with(quad(), ())
-        .with(translation(), vec3(0., 0., 0.5))
-        .spawn();
+pub fn subscribe() {
 
-    // for i in 0..10 {
-    //     Entity::new()
-    //     .with(name(), "Underwater Darkness Layer".to_string())
-    //     .with_merge(make_transformable())
-    //     .with(scale(), vec3(999., 999., 1.0))
-    //     .with(transparency_group(), 2)
-    //     .with(color(), vec4(0., 0., 0., 0.1 * i as f32))
-    //     .with(quad(), ())
-    //     .with(translation(), vec3(0., 0., -0.5 - 0.5 * i as f32))
-    //     .spawn();
-    // }
+    let parent_query: GeneralQuery<Component<()>> = query(is_mkworld_parent()).build();
 
+    crate::messages::GenerateWorld::subscribe(move |_src,_msg|{
+        let parent = spawn_parent(parent_query);
+        
+        // for i in 0..10+1 {
+        //     let depth = i as f32 * 0.1;
+        //     Entity::new()
+        //         .with(name(), "Water layer".to_string())
+        //         .with_merge(make_transformable())
+        //         .with(quad(), ())
+        //         .with(scale(), vec3(999., 999., 1.0))
+        //         .with(transparency_group(), 1)
+        //         .with(color(), vec4(
+        //             lerp(0.1, 0.0, depth), 
+        //             lerp(0.2, 0.0, depth), 
+        //             lerp(0.7, 0.0, depth), 
+        //             lerp(0.1, 1.0, depth),
+        //         ))
+        //         .with(translation(), vec3(0., 0., lerp(0.5, -0.5, depth)))
+        //         .spawn();
+        // }
         Entity::new()
-            .with(name(), "Water (2)".to_string())
+            .with(name(), "Water (1)".to_string())
             .with_merge(make_transformable())
             .with(scale(), vec3(999., 999., 1.0))
             .with(transparency_group(), 1)
-            .with(color(), vec4(0., 0., 0., 0.6))
+            .with(color(), vec4(0.4, 0.6, 0.9, 0.8))
             .with(quad(), ())
-            .with(translation(), vec3(0., 0., -4.))
+            .with(translation(), vec3(0., 0., 0.5))
             .spawn();
-        
-        Entity::new()
-            .with(name(), "Water (3)".to_string())
-            .with_merge(make_transformable())
-            .with(scale(), vec3(999., 999., 1.0))
-            .with(transparency_group(), 1)
-            .with(color(), vec4(0., 0., 0., 0.8))
-            .with(quad(), ())
-            .with(translation(), vec3(0., 0., -7.))
-            .spawn();
-        
+
+        // for i in 0..10 {
+        //     Entity::new()
+        //     .with(name(), "Underwater Darkness Layer".to_string())
+        //     .with_merge(make_transformable())
+        //     .with(scale(), vec3(999., 999., 1.0))
+        //     .with(transparency_group(), 2)
+        //     .with(color(), vec4(0., 0., 0., 0.1 * i as f32))
+        //     .with(quad(), ())
+        //     .with(translation(), vec3(0., 0., -0.5 - 0.5 * i as f32))
+        //     .spawn();
+        // }
+
             Entity::new()
-                .with(name(), "Water (4)".to_string())
+                .with(name(), "Water (2)".to_string())
                 .with_merge(make_transformable())
                 .with(scale(), vec3(999., 999., 1.0))
                 .with(transparency_group(), 1)
-                .with(color(), vec4(0., 0., 0., 0.9))
+                .with(color(), vec4(0., 0., 0., 0.6))
                 .with(quad(), ())
-                .with(translation(), vec3(0., 0., -9.))
+                .with(translation(), vec3(0., 0., -4.))
                 .spawn();
+            
+            Entity::new()
+                .with(name(), "Water (3)".to_string())
+                .with_merge(make_transformable())
+                .with(scale(), vec3(999., 999., 1.0))
+                .with(transparency_group(), 1)
+                .with(color(), vec4(0., 0., 0., 0.8))
+                .with(quad(), ())
+                .with(translation(), vec3(0., 0., -7.))
+                .spawn();
+            
+                Entity::new()
+                    .with(name(), "Water (4)".to_string())
+                    .with_merge(make_transformable())
+                    .with(scale(), vec3(999., 999., 1.0))
+                    .with(transparency_group(), 1)
+                    .with(color(), vec4(0., 0., 0., 0.9))
+                    .with(quad(), ())
+                    .with(translation(), vec3(0., 0., -9.))
+                    .spawn();
 
-    let pillars = Entity::new().with(name(), "Pillars".to_string()).spawn();
-    // for i in 0..30 {
-    //     let x = ((random::<u32>() % 10) as i32 - 5) * 2;
-    //     let y = ((random::<u32>() % 10) as i32 - 5) * 2;
-    //     entity::add_child(pillars, spawn_pillar_at(
-    //         ivec2(x,y).as_vec2().extend(0.0),
-    //         1.,
-    //         3. + 5. * random::<f32>()
-    //     ));
-    // }
-    
-    for x in -10..10 {
-        for y in -10..10 {
-            if (
-                (x % 2 == 0 || random::<f32>()<0.1)
-                &&
-                (y % 2 == 0 || random::<f32>()<0.1)
-            ) && random::<f32>()<0.9 {
-                let mut height = 2.0 + 1.6 * random::<f32>();
-                let mut depth = 20.0;
-                if random::<f32>() < 0.1 { height *= 1.0 + 5.0 * random::<f32>(); }
-                entity::add_child(pillars, spawn_pillar_at(
-                    ivec2(x,y).as_vec2().extend(0.0) * 2.,
-                    2.,
-                    height,
-                    depth,
-                ));
+        // for i in 0..30 {
+        //     let x = ((random::<u32>() % 10) as i32 - 5) * 2;
+        //     let y = ((random::<u32>() % 10) as i32 - 5) * 2;
+        //     entity::add_child(pillars, spawn_pillar_at(
+        //         ivec2(x,y).as_vec2().extend(0.0),
+        //         1.,
+        //         3. + 5. * random::<f32>()
+        //     ));
+        // }
+        
+        for x in -10..10 {
+            for y in -10..10 {
+                if (
+                    (x % 2 == 0 || random::<f32>()<0.1)
+                    &&
+                    (y % 2 == 0 || random::<f32>()<0.1)
+                ) && random::<f32>()<0.9 {
+                    entity::add_child(parent, spawn_building_stack_at(
+                        ivec2(x,y).as_vec2().extend(0.0) * 2.));
+                    // let mut height = 2.0 + 1.6 * random::<f32>();
+                    // let mut depth = 20.0;
+                    // if random::<f32>() < 0.1 { height *= 1.0 + 5.0 * random::<f32>(); }
+                    // entity::add_child(parent, spawn_pillar_at(
+                    //     ivec2(x,y).as_vec2().extend(0.0) * 2.,
+                    //     2.,
+                    //     height,
+                    //     depth,
+                    // ));
+                }
             }
         }
-    }
+
+    });
 }
 
 pub fn spawn_pillar_at(
@@ -119,12 +128,38 @@ pub fn spawn_pillar_at(
         .spawn();
 }
 
+pub fn spawn_building_stack_at(base_pos : Vec3) -> EntityId {
+    return Entity::new()
+        .with(name(), "Building Stack".to_string())
+        .with_merge(make_transformable())
+        .with(prefab_from_url(), asset::url("assets/MSH_Building_5x5x5_001.glb").unwrap())
+        // .with(cube(),())
+        // .with(cube_collider(), vec3(1.,1.,1.))
+        .with(translation(), base_pos + vec3(0.,0.,1.0))
+        // .with(scale(), vec3(width,width,height + depth))
+        .spawn();
+}
+
+pub fn spawn_parent(parent_query : GeneralQuery<Component<()>>)->EntityId {
+    // despawn all old parents (& children)
+    for(parent,_)in parent_query.evaluate(){entity::despawn_recursive(parent);}
+
+    Entity::new()
+        .with(name(), "Mkworld Parent".to_string())
+        .with(is_mkworld_parent(), ())
+        .spawn()
+}
+
 use ambient_api::prelude::*;
 use ambient_api::concepts::make_transformable;
 use ambient_api::components::core::{
     app::name,
     transform::{translation,scale},
+    prefab::prefab_from_url,
     physics::cube_collider,
     primitives::{cube, quad},
     rendering::{color,transparency_group},
 };
+use ambient_api::ecs::GeneralQuery;
+
+use crate::components::is_mkworld_parent;
--- w10_skatemouse/src/server.rs
+++ w10_skatemouse/src/server.rs
@@ -1,4 +1,4 @@
-use ambient_api::prelude::main;
+use ambient_api::prelude::*;
 
 mod s_selfie;
 mod s_playergliderphysics_v2_b;
@@ -14,10 +14,12 @@ pub fn main() {
     crate::s_selfie::setup();
     crate::s_playergliderphysics_v2_b::setup();
     crate::s_steering_and_camera::setup_mouse_control();
-    crate::s_mkworld::setup_world_entities();
     crate::s_cargo_snapping::setup();
     crate::s_deliveries::setup();
     crate::s_matter_buoyancy::setup();
+
+    crate::s_mkworld::subscribe();
+    messages::GenerateWorld{gen:true}.send_local_broadcast(true);
 
 }
 
